28d22641e42162162851c5ecfd9c37156b13655b
Shaan Shah <shahshaan@gmail.com>
2015-05-14 10:03:02 -0700

    Finishes arrays.indexOf to arrays.append

--- app/arrays.js
+++ app/arrays.js
@@ -3,23 +3,45 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    var result = -1;
+    for (var i = 0; i < arr.length; i++) {
+      var element = arr[i];
+      if (element === item) {
+        result = i;
+      }
+    }
+    return result;
   },
 
   sum : function(arr) {
-
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    };
+    return sum;
   },
 
   remove : function(arr, item) {
-
+    for (var i = (arr.length - 1); i >= 0; i--) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    for (var i = (arr.length - 1); i >= 0; i--) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr[arr['length']] = item;
+    return arr;
   },
 
   truncate : function(arr) {
